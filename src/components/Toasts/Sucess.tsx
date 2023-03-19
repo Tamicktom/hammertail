b1b5c1bceb66a52c2fc69319866b1c790044634f
Henrique <rickpokemon38@gmail.com>
2023-03-19 04:51:12 -0300

    feat - toast

---
+++ src/components/Toasts/Sucess.tsx
@@ -0,0 +1,47 @@
+//* Libraries imports
+import { CheckCircle, X } from 'phosphor-react';
+import { useState, useEffect } from 'react';
+import toast, { type Toast } from 'react-hot-toast';
+
+type Props = {
+  t: Toast,
+  topMsg: string,
+  bottomMsg: string
+}
+
+export default function Sucess({ t, topMsg, bottomMsg }: Props) {
+  const [visible, setVisible] = useState(t.visible);
+
+  useEffect(() => {
+    setVisible(t.visible);
+  }, [t.visible]);
+
+  return (
+    <div
+      className={`${visible ? "toast-enter" : "toast-leave"
+        } max-w-md bg-tertiary-800 rounded-lg flex flex-row p-2 border-2 border-sucess-500 min-h-14 gap-4`}
+      onAnimationEnd={() => {
+        if (!visible) {
+          toast.dismiss(t.id);
+        }
+      }}
+    >
+      <div className='flex justify-center items-center p-1'>
+        <CheckCircle className="w-8 h-8 text-sucess-500" size={32} />
+      </div>
+
+      <div className='flex flex-col justify-start items-start font-primary'>
+        <span className="font-normal text-sm text-sucess-100">{topMsg}</span>
+        <span className="font-black text-base text-sucess-500">{bottomMsg}</span>
+      </div>
+
+      <div className="w-10 flex justify-end items-start h-full">
+        <button
+          onClick={() => toast.dismiss(t.id)}
+        >
+          <X className="w-6 h-6 text-tertiary-700" size={24} />
+        </button>
+      </div>
+    </div>
+  )
+}
--- src/components/specific/WorldCreationModal/WorldCreationModal.tsx
+++ src/components/specific/WorldCreationModal/WorldCreationModal.tsx
@@ -2,17 +2,20 @@
 import { Root, Content, Overlay, Portal, Description, Close, Trigger, Title } from '@radix-ui/react-dialog';
 import { Pen, Plus, X } from 'phosphor-react';
 import { useState } from 'react';
+import toast from 'react-hot-toast';
 
 //* Types
 import type { World } from '@prisma/client';
 
 //* Components imports
 import WorldImage from '../WorldImage/WorldImage';
+import Sucess from '../../Toasts/Sucess';
 
 //* Store imports
 import worldStore from '../../../store/common/world';
 
 const WorldCreationModal = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const [worldName, setWorldName] = useState('');
   const [worldStartYear, setWorldStartYear] = useState(0);
   const [worldEndYear, setWorldEndYear] = useState(0);
@@ -25,16 +28,25 @@ const WorldCreationModal = () => {
       .then((world) => {
         if (world) {
           console.log("Mundo criado com sucesso!");
+          setIsModalOpen(false);
+          toast.custom((t) => (
+            <Sucess
+              t={t}
+              topMsg='Mundo criado com sucesso!'
+              bottomMsg='Comece a criar suas histórias!'
+            />
+          ));
           updateWorldList([...worldList, world]);
         }
       });
   }
 
   return (
-    <Root>
+    <Root open={isModalOpen}>
       <Trigger asChild>
         <button
           className="flex flex-row items-center justify-center gap-2 px-2 py-1 rounded-lg bg-gradient-to-b from-purple-500 to-purple-700"
+          onClick={() => setIsModalOpen(true)}
         >
           <Plus className="w-5 h-5 text-white" />
           <span className="font-bold text-white uppercase">Novo Mundo</span>
@@ -42,7 +54,7 @@ const WorldCreationModal = () => {
       </Trigger>
 
       <Portal>
-        <Overlay className='fixed inset-0 flex items-center justify-center DialogOverlay' />
+        <Overlay className='fixed inset-0 flex items-center justify-center DialogOverlay' onClick={() => { setIsModalOpen(false) }} />
 
         <Content className='DialogContent bg-white rounded-lg fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 max-w-md px-4 py-8 flex flex-col justify-start items-center gap-4'>
           <Title className='w-full text-center text-black font-bold text-2xl'>

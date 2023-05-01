5b89e8c566bdfd6b8571604406cab200d280590c
Tamicktom <rickpokemon38@gmail.com>
2023-05-01 20:33:28 -0300

    fix button

--- src/components/specific/RainbowButton/RainbowButton.tsx
+++ src/components/specific/RainbowButton/RainbowButton.tsx
@@ -9,6 +9,7 @@ export default function RainbowButton(props: Props) {
   return (
     <div className="rainbowButton w-full relative z-10">
       <button
+        type="button"
         onPointerDown={props.onPointerDown}
         className="p-0.5 rounded-lg w-full bg-rainbow flex justify-center items-center cursor-pointer h-10"
       >

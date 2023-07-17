235900e63ffc08d37d64d3bc35e2be82c65557c6
Tamicktom <rickpokemon38@gmail.com>
2023-07-16 21:39:02 -0300

    stage

--- src/components/specific/PageBackgroundImage/index.tsx
+++ src/components/specific/PageBackgroundImage/index.tsx
@@ -4,14 +4,16 @@ import Image from "next/image";
 import { useRouter } from "next/router";
 import Vibrant from 'node-vibrant';
 import colors from "tailwindcss/colors";
+import { useAtom } from "jotai";
 
 //* Hooks imports
 import usePage from "../../../hooks/queries/usePage";
 import useDebounce from "../../../hooks/common/useDebounce";
 
+//* Atom imports
+import { pageVibrantColorAtom } from "../../../store/common/page";
+
 type Props = {
-  color: [number, number, number];
-  setColor: (color: [number, number, number]) => void;
 }
 
 export default function PageBackgroundImage(props: Props) {
@@ -19,6 +21,7 @@ export default function PageBackgroundImage(props: Props) {
   const page = usePage(typeof router.query.index === "string" ? router.query.index : "");
   const pageImage = page.data?.image || "/login_screen_image.jpg";
   const debouncedPageImage = useDebounce(pageImage, 250);
+  const [pageColor, setPageColor] = useAtom(pageVibrantColorAtom);
 
   useMemo(() => {
     const v = new Vibrant(pageImage);
@@ -27,7 +30,7 @@ export default function PageBackgroundImage(props: Props) {
       // the color on the first position is in 0-1 range (porcent), but hsl
       // works in 0-360 range, so, we need to multiply it by 360
       const color2 = color ? [color[0] * 360, color[1] * 100, color[2] * 100 / 8] : [0, 0, 0];
-      props.setColor(color2 as [number, number, number]);
+      setPageColor(color2 as [number, number, number]);
     });
   }, [debouncedPageImage]);
 
@@ -45,7 +48,7 @@ export default function PageBackgroundImage(props: Props) {
       <div
         className='absolute bottom-0 left-0 w-full h-1/2 transition-all duration-500 ease-in-out'
         style={{
-          background: props.color ? `linear-gradient(180deg, transparent 0%, hsl(${props.color[0]}, ${props.color[1]}%, ${props.color[2]}%) 100%)` : `linear-gradient(180deg, transparent 0%, ${colors.neutral[800]} 100%)`
+          background: `linear-gradient(180deg, transparent 0%, hsl(${pageColor[0]}, ${pageColor[1]}%, ${pageColor[2]}%) 100%)`
         }}
       />
     </div>
---
+++ src/store/common/page.ts
@@ -0,0 +1,3 @@
+import { atom } from "jotai";
+
+export const pageVibrantColorAtom = atom<[number, number, number]>([0, 0, 0]);
